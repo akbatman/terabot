fb4ef794000e4812c7a0827e1611efa28e9ba7ea
akbatman <142990026+akbatman@users.noreply.github.com>
2024-05-16 17:19:25 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,24 +2,24 @@ from os import environ
 
 
 # BOT CONFIG
-API_ID = environ.get("API_ID", "25773387")  # api id
-API_HASH = environ.get("API_HASH", "d6315808e7741b4d04bb95877e0a8468")  # api hash
-BOT_TOKEN = environ.get("BOT_TOKEN", "7051688091:AAGnL-cHf3E0_MHj1RcJnFnE80sp77kIbkw")  # bot token
+API_ID = environ.get("API_ID", "28167530")  # api id
+API_HASH = environ.get("API_HASH", "202a9e8b13b7663417ddacc671420ad9")  # api hash
+BOT_TOKEN = environ.get("BOT_TOKEN", "6785693620:AAGeHHcw12M5qijCaTbEGV-Sge9gwdcrQwM")  # bot token
 
 # REDIS
-REDIS_HOST = environ.get("REDIS_HOST", "redis-13976.c239.us-east-1-2.ec2.cloud.redislabs.com")  # redis host uri
-REDIS_PORT = environ.get("REDIS_PORT", "13976")  # redis port
+REDIS_HOST = environ.get("REDIS_HOST", "redis-19981.c15.us-east-1-2.ec2.redns.redis-cloud.com")  # redis host uri
+REDIS_PORT = environ.get("REDIS_PORT", "19981")  # redis port
 REDIS_PASSWORD = environ.get(
-    "REDIS_PASSWORD", "J3RaGqWkhOaE7wRQ49xvBTvZ3EZzHZAr"
+    "REDIS_PASSWORD", "mHppSlUdGQYXEnwmAgzaAgSoYcQQdIZl"
 )  # redis password
 
 
 ADMINS = [6440245883,5015968435]
-OWNER_ID = 5491085270  # Replace with your Telegram user ID
+OWNER_ID = 6440245883  # Replace with your Telegram user ID
 PRIVATE_CHAT_ID = -1002021142183  # CHAT WHERE YOU WANT TO STORE VIDEOS
 USER_CHANNEL = -1001160455229
 DUMP_CHANNEL = -1001160455229
 
 
 # Config
-COOKIE = environ.get("COOKIE", "csrfToken=2frCVtuUrczo9gnmNG-VQCDD; browserid=AWdJ6JsHuCMYu2wZbr0ZgA3vuDhcyqA3LpkP0EQXYL1GCKhnM7Krz5AI84g; lang=en; TSID=apzc84KzRMsxUFj3NoOKo1BYSkIebp1j; __bid_n=18f80cd998bcfbe1cd4207; _ga=GA1.1.890766573.1715852910; ndus=Y4LZtCyteHuiZfamxYQIdTpDDKgNXMi8HStph09f; ndut_fmt=DD74DDDD4B41DD03546B27A6CBCF86C1B8DCFF44DFCA8BCE1FC3F792436A819B; _ga_06ZNKL8C2E=GGS1.1.1715852909.1.1.1715853213.60.0.0")
+COOKIE = environ.get("COOKIE", "csrfToken=tjtoLdkesqHiQZBC2SkONUKN; browserid=GAps1pg85rN5g0YDvglcd6ZQEFeMo7TokCR5YsaDjaiSflPKj2D53f74xsE; lang=en; TSID=Up5C4cTtOkGuj8B16MY6ZquXbTQF8daH; __bid_n=18f812ac399371f4ee4207; _ga=GA1.1.1015758587.1715859015; ndus=YyRNt3yteHuiHmmvokhUU2y2UwrQDsqpSSB408g5; ndut_fmt=EB796D5D7F63DCD33930CF62A48A9586BB94E070872510B1992D1F95E511B627; _ga_06ZNKL8C2E=GS1.1.1715859014.1.1.1715860025.44.0.0")
